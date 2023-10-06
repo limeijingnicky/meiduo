e6575137d1b188614bf0c9f71c993f32b924d495
limeijingnicky <limeijingnicky@gmail.com>
2023-10-06 22:57:34 +0900

    code changes

--- oauth/urls.py
+++ oauth/urls.py
@@ -3,6 +3,6 @@ from oauth import views
 
 
 urlpatterns = [
-    re_path(r'^qq/login', views.QQAuthURLView.as_view(),name='qqlogin'),
-    re_path(r'^oauth_callback/', views.QQAuthUserView.as_view(),name='qquser'),
+    re_path(r'^qq/login/$', views.QQAuthURLView.as_view(),name='qqlogin'),
+    re_path(r'^oauth_callback/$', views.QQAuthUserView.as_view(),name='qquser'),
 ]
